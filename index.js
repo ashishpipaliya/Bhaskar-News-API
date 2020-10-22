24cd7fa6e4312dc823147c1b538ebfb3cbe11a9e
Ashish <ashish.pipaliya22@gmail.com>
2020-10-22 13:16:01 +0530

    improved response

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const axios = require('axios');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get("/", async(req, res)=>{
+app.get("/", async (req, res) => {
     res.send({
-        "status" : "online"
+        "status": "online"
     });
 });
 
-app.get('/dainik/', async(req, res) => {
+// all news
+app.get('/hindi/', async (req, res) => {
 
     const url = "https://www.bhaskar.com/__api__/api/1.0/feed/home";
+
 
     var newsResponse = {};
     var articles = [];
 
-    axios.get(url, { dainikheaders }).then((response) => {
+    axios.defaults.headers = dainikHeaders;
+    axios.get(url).then((response) => {
 
 
         var articleCount = response.data.feed.length;
@@ -30,14 +35,14 @@ app.get('/dainik/', async(req, res) => {
 
         for (var i = 0; i < articleCount; i++) {
             articles.push({
-                "slug": response.data.feed[i].header ?.slug,
-                "headerTitle": response.data.feed[i] ?.header ?.title,
-                url: "https://www.bhaskar.com" + response.data.feed[i] ?.shortUrl,
-                "imageUrl": response.data.feed[i].header ?.media[0] ?.url ?? null,
-                "publishTime": response.data.feed[i] ?.publishTime,
-                "displayNameEn": response.data.feed[i] ?.category ?.nameEn,
-                "displayNameGuj": response.data.feed[i] ?.category ?.displayName,
-                "slugColor": response.data.feed[i] ?.category ?.color,
+                "slug": response.data.feed[i].header?.slug,
+                "headerTitle": response.data.feed[i]?.header?.title,
+                url: "https://www.bhaskar.com" + response.data.feed[i]?.shortUrl,
+                "imageUrl": response.data.feed[i].header?.media[0]?.url ?? null,
+                "publishTime": response.data.feed[i]?.publishTime,
+                "displayNameEn": response.data.feed[i]?.category?.nameEn,
+                "displayNameGuj": response.data.feed[i]?.category?.displayName,
+                "slugColor": response.data.feed[i]?.category?.color,
             });
         }
 
@@ -45,31 +50,30 @@ app.get('/dainik/', async(req, res) => {
 
         res.json(newsResponse);
 
-    }).catch(function(error) {
-        // console.log(error.message);
+    }).catch(function (error) {
+        console.log(error);
         res.send({
             status: error.status,
             message: error.message,
         });
     });
 
+
 });
 
 
 // category wise
-
-app.get('/dainik/:category', async(req, res) => {
+app.get('/hindi/:category', async (req, res) => {
 
     var category = req.params.category;
 
     const url = "https://www.bhaskar.com/__api__/api/1.0/feed/category/listingUrl/" + category;
 
-    // console.log(url);
-
     var newsResponse = {};
     var articles = [];
 
-    axios.get(url, { dainikheaders }).then((response) => {
+    axios.defaults.headers = dainikHeaders;
+    axios.get(url).then((response) => {
 
 
         var articleCount = response.data.feed.length;
@@ -82,14 +86,14 @@ app.get('/dainik/:category', async(req, res) => {
 
         for (var i = 0; i < articleCount; i++) {
             articles.push({
-                "slug": response.data.feed[i].header ?.slug,
-                "headerTitle": response.data.feed[i] ?.header ?.title,
-                url: "https://www.bhaskar.com" + response.data.feed[i] ?.shortUrl,
-                "imageUrl": response.data.feed[i].header ?.media[0] ?.url ?? null,
-                "publishTime": response.data.feed[i] ?.publishTime,
-                "displayNameEn": response.data.feed[i] ?.category ?.nameEn,
-                "displayNameGuj": response.data.feed[i] ?.category ?.displayName,
-                "slugColor": response.data.feed[i] ?.category ?.color,
+                "slug": response.data.feed[i].header?.slug,
+                "headerTitle": response.data.feed[i]?.header?.title,
+                url: "https://www.bhaskar.com" + response.data.feed[i]?.shortUrl,
+                "imageUrl": response.data.feed[i].header?.media[0]?.url ?? null,
+                "publishTime": response.data.feed[i]?.publishTime,
+                "displayNameEn": response.data.feed[i]?.category?.nameEn,
+                "displayNameGuj": response.data.feed[i]?.category?.displayName,
+                "slugColor": response.data.feed[i]?.category?.color,
             });
         }
 
@@ -97,8 +101,8 @@ app.get('/dainik/:category', async(req, res) => {
 
         res.json(newsResponse);
 
-    }).catch(function(error) {
-        // console.log(error.message);
+    }).catch(function (error) {
+        console.log(error.message + "line 111");
         res.send({
             status: error.name,
             message: error.message
@@ -109,15 +113,15 @@ app.get('/dainik/:category', async(req, res) => {
 
 
 // all news
-
-app.get('/divya/', async(req, res) => {
+app.get('/guj/', async (req, res) => {
 
     const url = "https://www.divyabhaskar.co.in/__api__/api/1.0/feed/home";
 
     var newsResponse = {};
     var articles = [];
 
-    axios.get(url, { divyaheaders }).then((response) => {
+    axios.defaults.headers = divyaHeaders;
+    axios.get(url).then((response) => {
 
         var articleCount = response.data.feed.length;
 
@@ -127,14 +131,14 @@ app.get('/divya/', async(req, res) => {
         for (var i = 0; i < articleCount; i++) {
 
             articles.push({
-                "slug": response.data.feed[i].header ?.slug,
-                "headerTitle": response.data.feed[i] ?.header ?.title,
-                "url": "https://www.divyabhaskar.co.in" + response.data.feed[i] ?.shortUrl,
-                "imageUrl": response.data.feed[i].header ?.media[0] ?.url ?? null,
-                "publishTime": response.data.feed[i] ?.publishTime,
-                "displayNameEn": response.data.feed[i] ?.category ?.nameEn,
-                "displayNameGuj": response.data.feed[i] ?.category ?.displayName,
-                "slugColor": response.data.feed[i] ?.category ?.color,
+                "slug": response.data.feed[i].header?.slug,
+                "headerTitle": response.data.feed[i]?.header?.title,
+                "url": "https://www.divyabhaskar.co.in" + response.data.feed[i]?.shortUrl,
+                "imageUrl": response.data.feed[i].header?.media[0]?.url ?? null,
+                "publishTime": response.data.feed[i]?.publishTime,
+                "displayNameEn": response.data.feed[i]?.category?.nameEn,
+                "displayNameGuj": response.data.feed[i]?.category?.displayName,
+                "slugColor": response.data.feed[i]?.category?.color,
             });
         }
 
@@ -144,29 +148,26 @@ app.get('/divya/', async(req, res) => {
 
         res.json(newsResponse);
 
-    }).catch(function(error) {
+    }).catch(function (error) {
         res.send({
             message: error.message
         });
     });
-
 });
 
 
 // category wise
-
-app.get('/divya/:category', async(req, res) => {
+app.get('/guj/:category', async (req, res) => {
 
     var category = req.params.category;
 
     const url = "https://www.divyabhaskar.co.in/__api__/api/1.0/feed/category/listingUrl/" + category;
 
-    // console.log(url);
-
     var newsResponse = {};
     var articles = [];
 
-    axios.get(url, { divyaheaders }).then((response) => {
+    axios.defaults.headers = divyaHeaders;
+    axios.get(url).then((response) => {
 
 
         var articleCount = response.data.feed.length;
@@ -181,14 +182,14 @@ app.get('/divya/:category', async(req, res) => {
             articles.push({
 
 
-                "slug": response.data.feed[i].header ?.slug,
-                "headerTitle": response.data.feed[i] ?.header ?.title,
-                "url": "https://www.divyabhaskar.co.in" + response.data.feed[i] ?.shortUrl,
-                "imageUrl": response.data.feed[i].header ?.media[0] ?.url ?? null,
-                "publishTime": response.data.feed[i] ?.publishTime,
-                "displayNameEn": response.data.feed[i] ?.category ?.nameEn,
-                "displayNameGuj": response.data.feed[i] ?.category ?.displayName,
-                "slugColor": response.data.feed[i] ?.category ?.color,
+                "slug": response.data.feed[i].header?.slug,
+                "headerTitle": response.data.feed[i]?.header?.title,
+                "url": "https://www.divyabhaskar.co.in" + response.data.feed[i]?.shortUrl,
+                "imageUrl": response.data.feed[i].header?.media[0]?.url ?? null,
+                "publishTime": response.data.feed[i]?.publishTime,
+                "displayNameEn": response.data.feed[i]?.category?.nameEn,
+                "displayNameGuj": response.data.feed[i]?.category?.displayName,
+                "slugColor": response.data.feed[i]?.category?.color,
 
             });
         }
@@ -197,14 +198,13 @@ app.get('/divya/:category', async(req, res) => {
 
         res.json(newsResponse);
 
-    }).catch(function(error) {
-        // console.log(error.message);
+    }).catch(function (error) {
+        console.log(error.message + "line 220");
         res.send({
             status: error.name,
             message: error.message
         });
     });
-
 });
 
 
@@ -259,8 +259,8 @@ app.get('/magic/', (req, res) => {
 
         res.json(newsResponse);
 
-    }).catch(function(error) {
-        console.log(error.message);
+    }).catch(function (error) {
+        console.log(error.message + "line 284");
     });
 
 });
@@ -274,37 +274,39 @@ app.listen(port, () => {
 });
 
 
-const dainikheaders = {
+
+
+var dainikHeaders = {
     "Host": "www.bhaskar.com",
     "Connection": "keep-alive",
     "DNT": 1,
     "x-aut-web-t": "420x66695ztde3qao6a69",
     "dtyp": "web",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
-    "rt": "3e e17c7871384e5183d1385769e2efdc",
-    "uid": 1312598760680128500,
+    "rt": "3ee17c7871384e5183d1385769e2efdc",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    "uid": "1312598760680128500",
     "at": "Gu3m3gVhYiR8d_iSCfG8ygDraa-ldBbISN9f2_pKlNHZf5gWaMOJ-F6cfMAGk8_cSVnDOzC0lCfTMwUGKC5TiGo8RbHDHa7UHp1-kB0AHavLEmRkzNbG0BRMsDspWhplXLodwr_f2Vgq_CrLV6rTfhPb6LJ8XX-bSXKkHqNDeLw0nBIIVSRmNrpd2F9UN7QvCcDFJb1JIAary29Oyx8lEIx6G22QHTDio9fdGFbGY2Y=",
-    "cid": 521,
+    "cid": "521",
     "Accept": "*/*",
     "Sec-Fetch-Site": "same-origin",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Dest": "empty",
-    "Referer": "https//www.bhaskar.com/",
+    "Referer": "https://www.bhaskar.com/",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9,gu;q=0.8,hi;q=0.7",
     "Cookie": "at=Gu3m3gVhYiR8d_iSCfG8ygDraa-ldBbISN9f2_pKlNHZf5gWaMOJ-F6cfMAGk8_cSVnDOzC0lCfTMwUGKC5TiGo8RbHDHa7UHp1-kB0AHavLEmRkzNbG0BRMsDspWhplXLodwr_f2Vgq_CrLV6rTfhPb6LJ8XX-bSXKkHqNDeLw0nBIIVSRmNrpd2F9UN7QvCcDFJb1JIAary29Oyx8lEIx6G22QHTDio9fdGFbGY2Y=; rt=3ee17c7871384e5183d1385769e2efdc; uid=1312598760680128500; bhaskarUUID=d6c3a4ee-b07a-86ba-e91c-b7e7aa332c49"
 };
 
 
-const divyaheaders = {
+var divyaHeaders = {
     "Host": "www.divyabhaskar.co.in",
     "Connection": "keep-alive",
     "DNT": 1,
     "x-aut-web-t": "420x66695ztde3qao6a69",
     "dtyp": "web",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
     "rt": "974ff17e1c6a40deaf328b975f1b4893",
-    "uid": 1312605706435920000,
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+    "uid": "1312605706435920000",
     "at": "Gu3m3gVhYiR8d_iSCfG8ygDraa-ldBbISN9f2_pKlNFrN2gRZh6k8IJwdHVuZG1azzr_1h10JoFDl6-aTPmevvAzDXkbgjI_vnqFDNHVnx4Fr3eWleCdRz8JNruIPuQFCYCmjaF93rkXjoUY9OJ1JpOkAyS9h5-dol8Q-8WNldwpDCd2N-yOB5ZvBwB7KwrqRN5FmwqhdY2b4pKcTue9RDviDx4aaGeT5wAR5l33eYc=",
     "cid": "960",
     "Accept": "*/*",
@@ -314,5 +316,5 @@ const divyaheaders = {
     "Referer": "https://www.divyabhaskar.co.in/",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "en-US,en;q=0.9,gu;q=0.8,hi;q=0.7",
-    "Cookie": "at=Gu3m3gVhYiR8d_iSCfG8ygDraa-ldBbISN9f2_pKlNFrN2gRZh6k8IJwdHVuZG1azzr_1h10JoFDl6-aTPmevvAzDXkbgjI_vnqFDNHVnx4Fr3eWleCdRz8JNruIPuQFCYCmjaF93rkXjoUY9OJ1JpOkAyS9h5-dol8Q-8WNldwpDCd2N-yOB5ZvBwB7KwrqRN5FmwqhdY2b4pKcTue9RDviDx4aaGeT5wAR5l33eYc=; rt=974ff17e1c6a40deaf328b975f1b4893; uid=1312605706435920000"
+    "Cookie": "at=Gu3m3gVhYiR8d_iSCfG8ygDraa-ldBbISN9f2_pKlNFrN2gRZh6k8IJwdHVuZG1azzr_1h10JoFDl6-aTPmevvAzDXkbgjI_vnqFDNHVnx4Fr3eWleCdRz8JNruIPuQFCYCmjaF93rkXjoUY9OJ1JpOkAyS9h5-dol8Q-8WNldwpDCd2N-yOB5ZvBwB7KwrqRN5FmwqhdY2b4pKcTue9RDviDx4aaGeT5wAR5l33eYc=; rt=974ff17e1c6a40deaf328b975f1b4893;uid=1312605706435920000"
 };
